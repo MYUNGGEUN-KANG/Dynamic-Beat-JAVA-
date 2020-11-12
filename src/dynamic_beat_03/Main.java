eed6f17d49be0e3ad1e2eb8a9d978b493f84e2a3
Oni-Nick <nicholas00105@gmail.com>
2020-11-12 19:58:27 +0900

    from local to remote github repo
    java rythme game (made at Nov 5)

--- src/dynamic_beat_03/Main.java
+++ src/dynamic_beat_03/Main.java
@@ -2,7 +2,7 @@ package dynamic_beat_03;
 
 public class Main {
 	
-	public static final int SCREEN_WITDH = 1280;
+	public static final int SCREEN_WITDH = 1280;	//commit좀 씨발
 	public static final int SCREEN_HEIGHT = 720;
 	
 	public static void main(String[] args) {
